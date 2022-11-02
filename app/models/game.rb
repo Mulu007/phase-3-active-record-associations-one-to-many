5a1eb09423b6307860483a8a9d19bae09ccf44a4
Mulu <brianmulu007@gmail.com>
2022-11-02 13:06:03 +0300

    Done

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,6 @@
 class Game < ActiveRecord::Base
-  
+  has_many :reviews
+
 end
+
+# Just like with belongs_to, following naming conventions is important: we use the plural for the has_many macro.
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,5 @@
 class Review < ActiveRecord::Base
-  
+  belongs_to :game
 end
+
+# Also, the name of the symbol we are passing to belongs_to must be singular:
